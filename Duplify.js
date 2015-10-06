d45403357dd18ca3e529c4850e037dfc3b11068a
Daniel <heberda@users.noreply.github.com>
2015-10-06 14:26:45 +0100

    Update Duplify.js

--- Duplify.js
+++ Duplify.js
@@ -4,13 +4,15 @@
 
         var opts = $.extend({}, $.fn.duplify.defaults, options);
 
-        $table.find('th:last').after('<th><span id="new-lineitem" class="glyphicon glyphicon-plus link-hover"></span></th>');
+        $table.find('th:last').after('<th><span id="new-lineitem" class="glyphicon glyphicon-plus ' + opts.newItemClass + '"></span></th>');
         $table.find('tbody tr:first').append($('<td>'));
+        $table.
+
 
         $("#new-lineitem").bind('click', function () {
             var $newRow = $(this).closest('table').find('tbody tr:first').clone().appendTo('tbody');
             $newRow.find('input').val('');
-            if (opts.canRemove) { $newRow.find('td:last').append('<span class="glyphicon glyphicon-remove remove-row link-hover"></span>'); }
+            if (opts.canRemove) { $newRow.find('td:last').append('<span class="glyphicon glyphicon-remove remove-row ' + opts.removeItemClass + '"></span>'); }
         });
 
         $table.closest('form').bind('submit', function () {
@@ -36,8 +38,10 @@
     // Plugin defaults – added as a property on our plugin function.
     $.fn.duplify.defaults = {
         canRemove: true,
-        htmlPrefix: undefined,
-        listName: undefined
+        htmlPrefix: 'StatementOfWork',
+        listName: 'LineItems',
+        newItemClass: 'link-hover',
+        removeItemClass: 'link-hover'
     };
 }
 )(jQuery);
