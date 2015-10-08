8a03e9d6e793c14dcc9511b1510a866acdfa7513
Daniel <heberda@users.noreply.github.com>
2015-10-08 10:39:17 +0100

    Support for existing rows and bin
    Previously if a user already had data those rows would not be remove-able
    
    In addition to this fix, when adding a new row AFTER removing one, if will retrieve it from the bin.

--- Duplify.js
+++ Duplify.js
@@ -1,26 +1,41 @@
 (function ($) {
     $.fn.duplify = function (options) {
         var $table = $(this);
+        var bin = [];
 
         var opts = $.extend({}, $.fn.duplify.defaults, options);
 
-        $table.find('th:last').after('<th><span id="new-lineitem" class="glyphicon glyphicon-plus ' + opts.newItemClass + '"></span></th>');
-        $table.find('tbody tr:first').append($('<td>'));
-        
-        $("#new-lineitem").bind('click', function () {
-            var $newRow = $(this).closest('table').find('tbody tr:first').clone().appendTo('tbody');
-            $newRow.find('input').val('');
-            if (opts.canRemove) { $newRow.find('td:last').append('<span class="glyphicon glyphicon-remove remove-row ' + opts.removeItemClass + '"></span>'); }
+        $table.find('th:last').after('<th style="max-width:20px;"><span class="glyphicon glyphicon-plus new-lineitem ' + opts.newItemClass + '"></span></th>');
+        $table.find('tbody tr').append($('<td>').css('max-width', '20px'));
+        var rows = $table.find('tbody tr');
+        var nRows = $(rows).length;
+        if (nRows > 1) {
+            $.each(rows, function (index, value) {
+                if (index !== 0)
+                    $(value).find('td:last').append('<span class="glyphicon glyphicon-remove remove-row ' + opts.removeItemClass + '"></span>');
+            });
+        }
+
+        $table.find(".new-lineitem").bind('click', function () {
+            if (bin.length > 0) {
+                $table.find('tbody').append(bin.pop());
+            } else {
+                var $newRow = $table.find('tbody tr:first').clone().appendTo($table.find('tbody'));
+                $newRow.find(':input').val('');
+                if (opts.canRemove) {
+                    $newRow.find('td:last').append('<span class="glyphicon glyphicon-remove remove-row ' + opts.removeItemClass + '"></span>');
+                }
+            }
         });
 
         $table.closest('form').bind('submit', function () {
-            var $rows = $('table tbody tr');
+            var $rows = $table.find('tbody tr');
             var count = 0;
             $.each($rows, function () {
                 var $inputs = $(this).find(':input');
                 $.each($inputs, function () {
                     var name = $(this).prop('id');
-                    $(this).prop('name', opts.htmlPrefix + '.' + listName + '[' + count + '].' + name);
+                    $(this).prop('name', opts.htmlPrefix + '.' + opts.listName + '[' + count + '].' + name);
                 });
                 count++;
             });
@@ -28,7 +43,8 @@
 
         if (opts.canRemove) {
             $(document).on('click', '.remove-row', function () {
-                $(this).closest('tr').remove();
+                bin.push($(this).closest('tr').detach());
+                //$(this).closest('tr').remove();
             });
         }
     };
